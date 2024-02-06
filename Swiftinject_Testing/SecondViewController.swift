1dbc4d9ea177017a009470f7aaa49a21e268e968
vishelpodishat <alishsaideshov@gmail.com>
2024-02-06 17:42:16 +0600

    init commit

---
+++ Swiftinject_Testing/SecondViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondViewController.swift
+//  Swiftinject_Testing
+//
+//  Created by Alisher Saideshov on 06.02.2024.
+//
+
+import Foundation
---
+++ Swiftinject_Testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftinject_Testing
+//
+//  Created by Alisher Saideshov on 06.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
